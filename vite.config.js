09b6136719821918b3f8a6a43217ca7dde960f52
Adam Collier <adamcollierart@gmail.com>
2021-05-17 17:37:00 +0100

    feature: use vites library mode to build components

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,39 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+const path = require('path');
+const packageJson = require('./package.json');
+
+// get all of our externals since to exclude from our bundle, they will be installed by the user
+const external = [
+  ...Object.keys({
+    ...(packageJson.dependencies || {}),
+    ...(packageJson.devDependencies || {}),
+    ...(packageJson.peerDependencies || {}),
+  }),
+  'prop-types',
+];
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
-})
+  plugins: [reactRefresh()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.js'),
+      name: 'viteStorybook',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external,
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'React',
+          'styled-components': "styled",
+          'prop-types': "PropTypes"
+        },
+      },
+    },
+  },
+});
