186d59b4f398c2095d62c4e3503ad8758f4b7f2e
Adam Collier <adamcollierart@gmail.com>
2021-05-17 17:39:16 +0100

    chore: add prop-types

--- src/components/Text/index.jsx
+++ src/components/Text/index.jsx
@@ -73,6 +73,39 @@ export const Text = (props) => {
   );
 };
 
+Text.propTypes = {
+  /**
+   * Boolean indicating whether the text should be a heading or not
+   */
+  heading: PropTypes.bool,
+  /** What alignment should the text have? */
+  align: PropTypes.oneOf(['left', 'center', 'right']),
+  /** How large should the text be */
+  size: PropTypes.oneOf([
+    '5xl',
+    '4xl',
+    '3xl',
+    '2xl',
+    'xl',
+    'lg',
+    'md',
+    'base',
+    'sm',
+    'xs',
+    'xxs',
+  ]),
+  /** Semantically what element should be used */
+  element: PropTypes.string,
+  /** What weight should the font be (only use if defaults don't satify) */
+  weight: PropTypes.number,
+  /** Optional line height (only use if defaults don't satify) */
+  lineHeight: PropTypes.number,
+  /** Text contents */
+  children: PropTypes.string,
+  /** The number of lines you want to truncate to */
+  truncate: PropTypes.number,
+};
+
 Text.defaultProps = {
   heading: false,
   element: 'p',
